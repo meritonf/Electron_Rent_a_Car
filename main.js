8f80693fc4022723b879c4a280f9d076a9b6f8c1
Meriton Ferati <meritonf@cacttus.local>
2017-10-24 22:33:59 +0200

    Fixing code for better understanding.

--- main.js
+++ main.js
@@ -2,22 +2,22 @@ const {app, BrowserWindow} = require('electron');
 const path = require('path');
 const url = require('url');
 
-let win;
+let window;
 
 function createWindow(){
-    win = new BrowserWindow({width:800, height: 600,icon:__dirname+'/img/sys.png'});
+    window = new BrowserWindow({width:800, height: 600,icon:__dirname+'/img/sys.png'});
     
-    win.loadURL(url.format({
+    window.loadURL(url.format({
         pathname: path.join(__dirname, 'index.html'),
         protocol: 'file:',
         slashes:true
     }));
     
     //open devtools
-    win.webContents.openDevTools();
+    window.webContents.openDevTools();
     
-    win.on('closed', ()=>{
-        win=null;
+    window.on('closed', ()=>{
+        window=null;
     })
 }
 //Run create window function
